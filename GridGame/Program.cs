41976b14f332e1d45c074497f8e525f4875b9128
PatrikFridh <37584660+PatrikFridh@users.noreply.github.com>
2018-10-09 11:01:11 +0200

    Big boi commit

--- GridGame/Program.cs
+++ GridGame/Program.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
+using System.IO;
 
 
 //Uppgift1: Skriv en ny klass Enemy, som ärver Gameobject och som vandrar runt slumpmässigt på spelplanen. Skapa en new Enemy och lägg till i GameObjects-listan.
 //Uppgift2: Skriv en ny klass Player, som ärver Gameobject och som kan styras med tangenterna WASD. Skapa en new Player och lägg till i GameObjects-listan.
 namespace GridGame
 {
-
+    
     class Program
     {
+
+    
         static void Main(string[] args)
         {
+
+            
+            
             Game myGame = new Game(15, 6);
+            Level level = new Level();
+            //RandomTestLevel rndLevel = new RandomTestLevel(10, 10);
+
+           // rndLevel.Draw(10, 10);
+
             while (true)
             {
                 myGame.UpdateBoard();
                 myGame.DrawBoard();
+                level.Start(0,0);
                 Console.ReadKey();
             }
 
@@ -30,19 +43,24 @@ namespace GridGame
     {
 
         List<GameObject> GameObjects = new List<GameObject>();
+        List<LevelBase> levels = new List<LevelBase>();
+        
 
         public Game(int xSize, int ySize)
         {
-            for (int i = 0; i < ySize + 2; i++)
-            {
-                for (int j = 0; j < xSize + 2; j++)
-                {
-                    if (j == 0 || i == 0 || i == ySize + 1 || j == xSize + 1)
-                    {
-                        GameObjects.Add(new Wall(j, i));
-                    }
-                }
-            }
+
+            //levels.Add(new RandomTestLevel(10, 10));
+
+            //for (int i = 0; i < ySize + 2; i++)
+            //{
+            //    for (int j = 0; j < xSize + 2; j++)
+            //    {
+            //        if (j == 0 || i == 0 || i == ySize + 1 || j == xSize + 1)
+            //        {
+            //            GameObjects.Add(new Wall(j, i));
+            //        }
+            //    }
+            //}
 
         }
 
@@ -52,6 +70,15 @@ namespace GridGame
             {
                 gameObject.Draw(5, 3);
             }
+
+            foreach(LevelBase level in levels)
+            {
+                
+            }
+
+            
+
+            
 
         }
 
@@ -85,12 +112,12 @@ namespace GridGame
         {
             int startX = XPosition * xBoxSize;
             int startY = YPosition * yBoxSize;
-            Console.SetCursorPosition(startX, startY);
-            Console.Write("█████");
-            Console.SetCursorPosition(startX, startY + 1);
-            Console.Write("█████");
-            Console.SetCursorPosition(startX, startY + 2);
-            Console.Write("█████");
+            //Console.SetCursorPosition(startX, startY);
+            //Console.Write("█");
+            //Console.SetCursorPosition(startX, startY + 1);
+            //Console.Write(" ██ ");
+            //Console.SetCursorPosition(startX, startY + 2);
+            //Console.Write("█  █");
         }
 
         public override void Update()
@@ -99,4 +126,198 @@ namespace GridGame
         }
     }
 
+    
+
+    class Level
+    {
+
+        Random rnd;
+
+        string ranLevel = File.ReadAllText(@"C:\Users\patrik.fridh\Documents\Github\GridGame\Test.txt");
+        char[] lines;
+        public Level()
+        {
+
+        }
+
+        public void Start(int startX, int startY)
+        {
+            int curY = startX;
+            int curX = startY;
+
+            rnd = new Random();
+
+            int rndNum = 0;
+
+            int offset = 1;
+
+            bool write = false;
+
+            List<Block> blocks = new List<Block>();
+
+            lines = ranLevel.ToCharArray(0, ranLevel.Length);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                rndNum = rnd.Next(3);
+
+                char curChar = lines[i];
+
+                if (curChar.ToString() == "1")
+                {
+                    write = true;
+                    curX += offset;
+                }
+                if (curChar.ToString() == "0")
+                {
+                    write = false;
+                    curX += offset;
+                }
+                if (curChar.ToString() == "2")
+                {
+                    write = false;
+                    curX = startX;
+                    curY += offset;
+                }
+                if(curChar.ToString() == "3")
+                {
+                    if(rndNum == 1)
+                    {
+                        write = true;
+                    }
+                    else
+                    {
+                        write = false;
+                    }
+
+                    curX += offset;
+                }
+
+
+                blocks.Add(new Block(curX, curY, write));
+                blocks[i].Draw(curX, curY);
+            }
+        }       
+    }
+
+    class Block : GameObject
+    {
+        int xPosition;
+        int yPosition;
+        
+        bool write;
+
+        public Block(int xPos, int yPos, bool ifWrite)
+        {
+            xPosition = xPos;
+            yPosition = yPos;
+            write = ifWrite;
+        }
+
+
+        public override void Update()
+        {
+
+        }
+
+        public override void Draw(int x, int y)
+        {
+            
+            if (write)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write("█");
+                //Console.SetCursorPosition(x, y + 1);
+                //Console.Write("█");
+            }
+            else
+            {
+                Console.Write("    "); 
+            }           
+        }
+    }
+
+    abstract class LevelBase
+    {
+
+        public abstract void Draw(int x, int y);
+
+    }
+
+    class RandomTestLevel : LevelBase
+    {
+        int levelXSize;
+        int levelYSize;
+
+        bool RIGHT = true;
+        bool LEFT = true;
+        bool UP = true;
+        bool Down = true;
+
+        Random rnd = new Random();
+
+        int curX;
+        int curY;
+
+        bool done = false;
+
+        int moves = 0;
+
+        public RandomTestLevel(int xSize, int ySize)
+        {
+            levelXSize = xSize;
+            levelYSize = ySize;
+
+            while (!done)
+            {
+                int dir = rnd.Next(4);
+
+                //Console.WriteLine(dir);
+
+                if (dir == 0 && curX < 100)
+                {
+                    curX++;
+                }
+                if (dir == 1 && curX > 0 && curX < 100)
+                {
+                    curX--;
+                }
+                if (dir == 2 && curY < 100)
+                {
+                    curY++;
+                }
+                if (dir == 3 && curY > 0 && curX < 100)
+                {
+                    curY--;
+                }
+                else
+                {
+                    curX++;
+                }
+
+                Draw(curX, curY);
+
+                moves++;
+
+                if (moves == 100)
+                {
+                    done = true;
+                }
+            }
+
+        }
+
+        public override void Draw(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("████");
+            Console.SetCursorPosition(x, y + 1);
+            Console.Write("████");
+        }
+
+        
+
+
+
+    }
 }
